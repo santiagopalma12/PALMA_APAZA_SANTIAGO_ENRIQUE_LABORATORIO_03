429cde322575f711fce67240d98933e6b4fbee86
santiagopalma12 <spalmaa@unsa.edu.pe>
2024-10-05 17:02:35 -0500

    Se mplementa la creación de naves y permite al usuario configurar nombre, posición, estado y puntos. Ahora añade el metodo para mostrar las naves creadas con sus atributos.

---
+++ DemoBatalla.java
@@ -0,0 +1,39 @@
+package labFP2;
+
+
+import java.util.*;
+public class DemoBatalla {
+    public static void main(String[] args) {
+        Nave[] misNaves = new Nave[10];
+        Scanner sc = new Scanner(System.in);
+        String nomb, col;
+        int fil, punt;
+        boolean est;
+
+        for (int i = 0; i < misNaves.length; i++) {
+            System.out.println("Nave " + (i + 1));
+            System.out.print("Nombre: ");
+            nomb = sc.next();
+            System.out.print("Fila: ");
+            fil = sc.nextInt();
+            System.out.print("Columna: ");
+            col = sc.next();
+            System.out.print("Estado (true/false): ");
+            est = sc.nextBoolean();
+            System.out.print("Puntos: ");
+            punt = sc.nextInt();
+
+            misNaves[i] = new Nave(); 
+
+            misNaves[i].setNombre(nomb);
+            misNaves[i].setFila(fil);
+            misNaves[i].setColumna(col);
+            misNaves[i].setEstado(est);
+            misNaves[i].setPuntos(punt);
+        }
+
+        System.out.println("\nNaves creadas:");
+        //mostrarNaves(misNaves);
+    }
+
+}
